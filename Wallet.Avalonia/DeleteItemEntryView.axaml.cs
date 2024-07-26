39f9442c50493aab435a682b6d6b89b65caca49f
TheXamlGuy <dan_clark@outlook.com>
2024-07-26 21:45:22 +0100

    SettingsExpander fixes

---
+++ Wallet.Avalonia/DeleteItemEntryView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Wallet.Avalonia;
+
+public partial class DeleteItemEntryView : UserControl
+{
+    public DeleteItemEntryView()
+    {
+        InitializeComponent();
+    }
+}
